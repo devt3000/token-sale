f2c4fca1ad00c97a9b2d2798f45becd22cfb6151
Stanley Makazhe <devt3000@gmail.com>
2018-08-20 18:23:25 +0200

    Allocating totalSupply to admin

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -3,10 +3,14 @@ pragma solidity ^0.4.2;
 contract DappToken {
     // Constructor
     uint256 public totalSupply;
-    
-    function DappToken () public {
-        totalSupply = 1000000;
+
+    mapping(address => uint256) public balanceOf;
+
+    function DappToken (uint256 _initialSupply) public {
+        balanceOf[msg.sender] = _initialSupply;
+        totalSupply = _initialSupply;
     }
     // Set the total number of tokens
     // Read the total number of tokens
+    // allocate the initial supply
 }
