1ffdb8a44d5fc4b07531a0fc9e83c53879de96a8
Stanley Makazhe <devt3000@gmail.com>
2018-08-20 16:49:03 +0200

    Adding constructor and initial supply

---
+++ contracts/DappToken.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.4.2;
+
+contract DappToken {
+    // Constructor
+    uint256 public totalSupply;
+    
+    function DappToken () public {
+        totalSupply = 1000000;
+    }
+    // Set the total number of tokens
+    // Read the total number of tokens
+}
