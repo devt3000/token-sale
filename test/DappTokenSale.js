4f51ba1db1101dcd13366dc13ff11159bd0d12a9
Stanley Makazhe <devt3000@gmail.com>
2018-08-23 21:53:18 +0200

    Testing tokensSold, sale event and multiply function

--- test/DappTokenSale.js
+++ test/DappTokenSale.js
@@ -2,7 +2,9 @@ var DappTokenSale = artifacts.require('./DappTokenSale.sol')
 
 contract('DappTokenSale', function(accounts) {
     var tokenSaleInstance;
+    var buyer = accounts[1];
     var tokenPrice = 1000000000000000000; // wei
+    var numberOfTokens;
 
     it('initializes the contract with the correct values', function() {
         return DappTokenSale.deployed().then(function(instance) {
@@ -19,9 +21,23 @@ contract('DappTokenSale', function(accounts) {
         });
     });
 
-    it('facilitates toen buying', function() {
+    it('facilitates token buying', function() {
         return DappTokenSale.deployed().then(function(instance) {
             tokenSaleInstance = instance;
+            numberOfTokens = 10;
+            return tokenSaleInstance.buyTokens(numberOfTokens, { from: buyer, value: numberOfTokens * tokenPrice })
+        }).then(function(receipt) {
+            assert.equal(receipt.logs.length, 1, 'triggers one event');
+            assert.equal(receipt.logs[0].event, 'Sell', 'should be the "Sell" event');
+            assert.equal(receipt.logs[0].args._buyer, buyer, 'logs the account that purchased the tokens');
+            assert.equal(receipt.logs[0].args._amount, numberOfTokens, 'logs the number of tokens purchased');
+            return tokenSaleInstance.tokensSold();
+        }).then(function(amount) {
+            assert.equal(amount.toNumber(), numberOfTokens, 'increment the number of tokens sold');
+            // Try to buy toens different from the ether vlue
+            return tokenSaleInstance.buyTokens(numberOfTokens, { from: buyer, value: 1 });
+        }).then(assert.fail).catch(function(error) {
+            assert(error.message.indexOf('revert') >= 0, 'msg.value must equal number of tokens in wei');
         })
-    })
+    });
 });
