f43ce634244b8f197b261e0ee737c4931498fd3d
Stanley Makazhe <devt3000@gmail.com>
2018-08-24 19:11:49 +0200

    Testing ending the Token sale

--- test/DappTokenSale.js
+++ test/DappTokenSale.js
@@ -80,6 +80,10 @@ contract('DappTokenSale', function(accounts) {
             return tokenInstance.balanceOf(admin);
         }).then(function(balance) {
             assert.equal(balance.toNumber(), 999990, 'returns all unsold dapp tokens to admin');
+            // Check that token price was reset when selfDestruct was called
+            return tokenSaleInstance.tokenPrice();
+        }).then(function(price) {
+            assert.equal(price.toNumber(), 0, 'token price was reset');
         })
     })
 });
